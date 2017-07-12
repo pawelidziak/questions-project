ae8197b6e32beab42ad7e45306b06403aee8c99c
pawel.idziak <208766@student.pwr.edu.pl>
2017-07-12 15:01:14 +0200

    Add voting service, changed comment to one component and add comments-list.component

---
+++ src/app/questions-list/question-detail/answer/comments-list/comments-list.component.ts
@@ -0,0 +1,18 @@
+import {Component, Input, OnInit} from '@angular/core';
+import {IResponse} from '../../../../_classes/Response';
+
+@Component({
+  selector: 'app-comments-list',
+  templateUrl: './comments-list.component.html',
+  styleUrls: ['./comments-list.component.sass']
+})
+export class CommentsListComponent implements OnInit {
+
+  @Input('comments') comments: IResponse[];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
